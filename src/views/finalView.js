bc6d3abed7d2c5fae714015bbee698d52ebfad9a
konjit <kkll6349@gmail.com>
2025-01-18 13:34:09 +0100

    Ui imporved

--- src/views/finalView.js
+++ src/views/finalView.js
@@ -1,4 +1,8 @@
-import { RESTART_QUIZ_BUTTON_ID } from '../constants.js';
+import {
+  GREET_DIV_ID,
+  RESTART_QUIZ_BUTTON_ID,
+  FINAL_MSG_ID,
+} from '../constants.js';
 
 /**
  * Create a full question element
@@ -6,14 +10,15 @@ import { RESTART_QUIZ_BUTTON_ID } from '../constants.js';
  */
 export const createFinalElement = (score, question) => {
   const element = document.createElement('div');
+  element.id = GREET_DIV_ID;
   let message = !window.remainingTime ? `Time's Up!` : '';
 
   // I use String.raw just to get fancy colors for the HTML in VS Code.
   element.innerHTML = String.raw`
-    <h1>${message}
+    <h1 id="${FINAL_MSG_ID}">${message} <span class="highlight">
     ${localStorage.getItem(
       'userName'
-    )}. You scored ${score} out of ${question}!</h1>
+    )}</span>, You scored ${score} out of ${question}!</h1>
 
     <button id="${RESTART_QUIZ_BUTTON_ID}">
      Restart Quiz
